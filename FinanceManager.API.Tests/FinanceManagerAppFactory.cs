1cc8a181e67fc1306c650206dd909dc1c77f1dc0
Michal Brejnakowski <mbrejnakowski24@gmail.com>
2021-05-21 18:26:23 +0200

    Added integration tests configuration

---
+++ FinanceManager.API.Tests/FinanceManagerAppFactory.cs
@@ -0,0 +1,36 @@
+﻿using FinanceManager.API.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace FinanceManager.API.IntegrationTests
+{
+    public class FinanceManagerAppFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+
+            builder.ConfigureServices(services =>
+            {
+                var databaseToRemove = services.SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<DataContext>));
+                services.Remove(databaseToRemove);
+
+                services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("TestDb"));
+
+                var serviceProvider = services.BuildServiceProvider();
+
+                using (var scope = serviceProvider.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var testDb = scopedServices.GetRequiredService<DataContext>();
+
+                    testDb.Database.EnsureCreated();
+                }
+            });
+        }
+    }
+}
---
+++ FinanceManager.API.Tests/IntegrationTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FinanceManager.API.IntegrationTests
+{
+    public class IntegrationTest : IClassFixture<FinanceManagerAppFactory<Startup>>
+    {
+        protected readonly FinanceManagerAppFactory<Startup> _financeManagerAppFactory;
+
+        protected IntegrationTest(FinanceManagerAppFactory<Startup> financeManagerAppFactory)
+        {
+            _financeManagerAppFactory = financeManagerAppFactory;
+        }
+    }
+}
